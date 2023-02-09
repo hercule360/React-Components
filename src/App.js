2dfba65601ade0bb05842999616e82d7103a3560
hercule360 <jihed.gharbi0@gmail.com>
2023-02-09 10:42:08 +0100

    mise ajour

--- src/App.js
+++ src/App.js
@@ -10,9 +10,9 @@ function App() {
       <Navb />
      <Title props={styleProp} />
      <div className='cards'>
-    <Product mark='ATHOL MGHREB' name='plante pomme '> < img src='https://minimum-design.com/wp-content/uploads/2019/05/cache-pot-bois-noir-design-geometrique-et-minimaliste-apple-decoration-scandinave-5cd02a82.jpg' alt='abc' />  </Product>
-    <Product mark='ETHIQ PARIS' name='plante ETHIQ '> < img src='https://www.e-thiq.fr/2964/pot-pour-plantes-apple-naturel.jpg' alt='abc' />  </Product>
-    <Product mark='ETniy UK' name='plante etniy '> < img src='https://i.etsystatic.com/13346155/c/2268/2268/0/0/il/40e6e5/1889697591/il_300x300.1889697591_n2q6.jpg' alt='abc' />  </Product>
+    <Product mark='ATHOL MGHREB'  country ='Tunis'   name='plante pomme '> < img src='https://minimum-design.com/wp-content/uploads/2019/05/cache-pot-bois-noir-design-geometrique-et-minimaliste-apple-decoration-scandinave-5cd02a82.jpg' alt='abc' />  </Product>
+    <Product mark='ETHIQ PARIS'  country ='France'   name='plante ETHIQ '> < img src='https://www.e-thiq.fr/2964/pot-pour-plantes-apple-naturel.jpg' alt='abc' />  </Product>
+    <Product mark='ETniy UK'   country ='Irlend IKEA'  name='plante etniy '> < img src='https://i.etsystatic.com/13346155/c/2268/2268/0/0/il/40e6e5/1889697591/il_300x300.1889697591_n2q6.jpg' alt='abc' />  </Product>
     </div>
     
     
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,20 +1,29 @@
 import React from 'react'
 import { Button, Card } from 'react-bootstrap'
-
+import Prpostypes from 'prop-types'
 const Product = (props) => {
   return (
     <div>
-         <Card style={{ width: '18rem' }}>
+       <Card style={{ width: '18rem' }}>
 {props.children}    <Card.Body>
         <Card.Title>{props.name}</Card.Title>
         <Card.Text>
-        Vous êtes à une touche de votre jungle personnelle - Prenez une photo de votre plante et Planta s'occupe du reste !
+        {props.country}
         </Card.Text>
         <Button onClick={()=>alert(`the product mark is ${props.mark}`)} variant="primary">Go planet vert </Button>
       </Card.Body>
     </Card> 
     </div>
   )
+}
+
+Product.Prpostypes= {
+  mark: Prpostypes.string,
+  name: Prpostypes.string,
+}
+
+Product.defaultPropos= {
+  country :  'France'
 }
 
 export default Product
